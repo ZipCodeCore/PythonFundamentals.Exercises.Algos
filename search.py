25a98cea2e59f7df14853c6b6854b57363f527c1
rpdedeus <roberto.p.dedeus@gmail.com>
2020-04-20 10:02:45 -0400

    Add binary search.

---
+++ search.py
@@ -0,0 +1,9 @@
+from typing import List, Optional
+import logging
+
+
+def binary_search(list_in: List[int], item: int) -> Optional[int]:
+    """
+    If item exists in list_in, this function returns its position in the list.
+    """
+    pass
---
+++ test_search.py
@@ -0,0 +1,12 @@
+import unittest
+import search
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+class SearchTest(unittest.TestCase):
+
+    def test_binary_search(self):
+        pass
+
